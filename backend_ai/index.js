4e671695ac814bc9d19f1b4e0b16aff33039668b
Jyoti Patel <jyotipatel10304@gmail.com>
2026-04-18 15:42:37 +0530

    fix backend render deployment

--- backend_ai/index.js
+++ backend_ai/index.js
@@ -1,37 +1,31 @@
+require("dotenv").config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 4000;
- 
-require("dotenv").config();
-const path = require('path');
-
+const PORT = process.env.PORT || 4000;
 
 require('./conn');
+
 app.use(express.json());
 app.use(cors({
-  credentials: true,
-  origin: [
-    "http://localhost:5173",
-    "https://your-frontend-app.vercel.app"
-  ]
+    credentials: true,
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:4000"
+    ]
 }));
 
 const UserRoute = require('./Routes/user');
 const ResumeRoute = require('./Routes/resume');
 
-
 app.use('/api/user', UserRoute);
 app.use('/api/resume', ResumeRoute);
 
- // Serve static files from the build folder
- app.use(express.static(path.join(__dirname, "build")));
-
-// // Catch-all route: send index.html for React Router
- app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
- });
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
 
 app.listen(PORT, () => {
-    console.log("Server is running on port ",  PORT);
+    console.log("Server is running on port ", PORT);
 });
