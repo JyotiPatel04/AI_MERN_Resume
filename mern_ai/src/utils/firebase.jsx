036b17829b968c4d45d99f3a35c5572d2a6a11a0
Jyoti Patel <jyotipatel10304@gmail.com>
2026-04-18 16:26:27 +0530

    fix frontend

--- mern_ai/src/utils/firebase.jsx
+++ mern_ai/src/utils/firebase.jsx
@@ -1,21 +1,20 @@
 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_DgX5nWQAwcJo407lC4WCikdBRkJnlXk",
-  authDomain: "mernai-1e690.firebaseapp.com",
-  projectId: "mernai-1e690",
-  storageBucket: "mernai-1e690.firebasestorage.app",
-  messagingSenderId: "983362240899",
-  appId: "1:983362240899:web:541e83b1817e4b440af6b6",
-  measurementId: "G-3F63ETP6V3"
+  apiKey: "AIzaSyBMiZsCgrUlkbF7BzFRPjLW7WR0Jd38IhU",
+  authDomain: "mernai-fb6a2.firebaseapp.com",
+  projectId: "mernai-fb6a2",
+  storageBucket: "mernai-fb6a2.firebasestorage.app",
+  messagingSenderId: "455394868446",
+  appId: "1:455394868446:web:2529d71145bc9de028964f",
+  measurementId: "G-DMHSQPCJ0S"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export {auth, provider};
+export { auth, provider };
